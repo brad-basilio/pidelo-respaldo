c41ef18eb42f80eb6162c935725ae1a4e3bc03f6
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2024-12-20 12:10:12 -0500

    Subiendo nuevo banner

--- resources/js/Components/Tailwind/Banners/BannerFullWidth.jsx
+++ resources/js/Components/Tailwind/Banners/BannerFullWidth.jsx
@@ -14,13 +14,14 @@ const BannerFullWidth = ({ data }) => {
           <p className="text-white mb-4">{data?.description}</p>
           {
             (data?.button_link && data?.button_text) &&
-            <button href={data?.button_link} className="px-3 py-2 rounded-full bg-white text-textPrimary shadow-md">
+            <button href={data?.button_link} className="px-4 py-2 rounded-full bg-primary text-white shadow-md">
               {data?.button_text}
+              <i className="mdi mdi-arrow-top-right ms-2"></i>
             </button>
           }
         </div>
         <div className="flex items-center justify-center">
-          <img src={`/api/banners/media/${data?.image}`} className="h-full aspect-square object-contain max-h-[320px]" alt="" />
+          <img src={`/api/banners/media/${data?.image}`} className="w-full aspect-auto object-contain object-bottom" alt="" />
         </div>
       </div>
     </div>
