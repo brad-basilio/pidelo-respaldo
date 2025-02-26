12598da0e9303b49077f3bc72e46d613f0761a19
brad-basilio <brad.basilioppy@gmail.com>
2025-02-26 12:38:14 -0500

    SignUpSimple

--- resources/js/Components/Tailwind/Signup.jsx
+++ resources/js/Components/Tailwind/Signup.jsx
@@ -2,14 +2,14 @@ import React from "react"
 
 
 
-const SignupSimple = React.lazy(() => import('./Signup/SignupSimple'))
+const SignUpSimple = React.lazy(() => import('./Signup/SignUpSimple'))
 
 const Signup = ({ data, which }) => {
     const getSignup = () => {
         switch (which) {
 
-            case 'SignupSimple':
-                return <SignupSimple data={data} />
+            case 'SignUpSimple':
+                return <SignUpSimple data={data} />
             default:
                 return <div className="w-full px-[5%] replace-max-w-here p-4 mx-auto">- No Hay componente <b>{which}</b> -</div>
         }
