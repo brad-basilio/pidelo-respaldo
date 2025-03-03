c737bb5f5571c56b9f893909a364d2e4c5054776
brad-basilio <brad.basilioppy@gmail.com>
2025-03-03 17:31:39 -0500

    scraper

--- storage/app/scraper/scraper-invictastores.cjs
+++ storage/app/scraper/scraper-invictastores.cjs
@@ -772,9 +772,9 @@ puppeteer.use(AnonymizeUAPLugin());
         });
 
         // Configurar User-Agent real
-        await page.setUserAgent(
+        /* await page.setUserAgent(
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-        );
+        );*/
 
         // Desactivar detección de WebDriver
         await page.evaluateOnNewDocument(() => {
