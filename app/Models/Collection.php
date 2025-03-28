c848db358cbb5a81b949e2968ff7e51de90c8f01
brad-basilio <brad.basilioppy@gmail.com>
2025-03-28 11:28:27 -0500

    collections

---
+++ app/Models/Collection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Collection extends Model
+{
+    use HasFactory, HasUuids;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'slug',
+        'name',
+        'description',
+        'image',
+        'featured',
+        'visible',
+        'status',
+    ];
+}
---
+++ resources/js/Actions/Admin/CollectionsRest.js
@@ -0,0 +1,8 @@
+import BasicRest from "../BasicRest";
+
+class CollectionsRest extends BasicRest {
+    path = "admin/collections";
+    hasFiles = true;
+}
+
+export default CollectionsRest;
