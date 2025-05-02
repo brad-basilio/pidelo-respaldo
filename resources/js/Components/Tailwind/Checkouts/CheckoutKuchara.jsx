c27ef8f98916d1b4adb09de98b48be429ed3bd1b
Manuel Gamboa <gamboapalominocarlosmanuel@gmail.com>
2025-05-02 14:21:18 -0500

    Calculo del igv en base al total

--- resources/js/Components/Tailwind/Checkouts/CheckoutKuchara.jsx
+++ resources/js/Components/Tailwind/Checkouts/CheckoutKuchara.jsx
@@ -192,6 +192,8 @@ const CheckoutCulqi = ({ data, cart, setCart, items, prefixes }) => {
 
   const igv = Number(General.igv_checkout) / 100
 
+  const finalPrice = totalPrice + (typeof shippingPrice === 'number' ? shippingPrice : 0)
+
   return (
     <form className="bg-white" onSubmit={onCheckoutSubmit}>
       <div className="container mx-auto px-[5%] py-[2.5%]">
@@ -742,20 +744,20 @@ const CheckoutCulqi = ({ data, cart, setCart, items, prefixes }) => {
 
                 <div className="flex justify-between items-center">
                   <p>Subtotal</p>
-                  <span>S/. {Number2Currency(igv > 0 ? totalPrice * (1 - igv) : totalPrice)}</span>
+                  <span>S/. {Number2Currency(igv > 0 ? finalPrice * (1 - igv) : finalPrice)}</span>
                 </div>
 
                 {
                   igv > 0 &&
                   <div className="flex justify-between items-center">
                     <p>IGV ({(igv * 100).toFixed(2)}%)</p>
-                    <span>S/. {Number2Currency(totalPrice * igv)}</span>
+                    <span>S/. {Number2Currency(finalPrice * igv)}</span>
                   </div>
                 }
 
                 <div className="flex justify-between items-center font-semibold text-lg pt-2 border-t border-gray-200">
                   <p>Total</p>
-                  <span>S/ {Number2Currency(totalPrice + (typeof shippingPrice === 'number' ? shippingPrice : 0))}</span>
+                  <span>S/ {Number2Currency(finalPrice)}</span>
                 </div>
 
                 <button
