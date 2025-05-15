4292ae3dbb74f19f31858507f5e25dfbc5823cca
brad-basilio <brad.basilioppy@gmail.com>
2025-05-14 20:12:27 -0500

    menu atualizado

--- resources/js/Components/Tailwind/Menu/MenuSimple.jsx
+++ resources/js/Components/Tailwind/Menu/MenuSimple.jsx
@@ -3,61 +3,53 @@ import { useEffect, useRef, useState } from "react";
 
 const MenuSimple = ({ pages = [], items }) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-
     const menuRef = useRef(null);
 
     useEffect(() => {
         function handleClickOutside(event) {
             if (menuRef.current && !menuRef.current.contains(event.target)) {
-                setIsOpen(false);
+                setIsMenuOpen(false);
             }
         }
-        document.addEventListener("mousedown", handleClickOutside);
-        return () =>
-            document.removeEventListener("mousedown", handleClickOutside);
+        document.addEventListener("pointerdown", handleClickOutside);
+        return () => document.removeEventListener("pointerdown", handleClickOutside);
     }, []);
 
     return (
-        <nav className="hidden md:block bg-secondary font-font-secondary font-normal text-sm">
-            <div className="px-primary 2xl:px-0 2xl:max-w-7xl mx-auto ">
-                <ul className="flex items-center gap-6 text-sm" ref={menuRef}>
+        <nav className="hidden md:block bg-secondary font-font-secondary font-normal text-sm" ref={menuRef}>
+            <div className="px-primary 2xl:px-0 2xl:max-w-7xl mx-auto">
+                <ul className="flex items-center gap-6 text-sm">
                     <li className="relative py-3">
                         <button
-                            className="customtext-neutral-dark flex items-center gap-2 hover:customtext-primary  pr-6 transition-colors duration-300 relative before:absolute before:right-0 before:top-1/2 before:-translate-y-1/2 before:h-3 before:w-[1px] before:bg-[#262624]"
+                            className="customtext-neutral-dark flex items-center gap-2 hover:customtext-primary pr-6 transition-colors duration-300 relative before:absolute before:right-0 before:top-1/2 before:-translate-y-1/2 before:h-3 before:w-[1px] before:bg-[#262624]"
                             onClick={() => setIsMenuOpen(!isMenuOpen)}
                         >
                             Categorias
-                            {isMenuOpen ? (
-                                <i className="mdi mdi-chevron-up"></i>
-                            ) : (
-                                <i className="mdi mdi-chevron-down"></i>
-                            )}
+                            {isMenuOpen ? <ChevronUp size={20} /> : <ChevronDown size={20} />}
                         </button>
                         {isMenuOpen && (
-                            <div className="absolute z-50 top-12 left-0 bg-white shadow-xl border-t rounded-xl transition-all duration-500 ease-in-out min-w-[900px] w-auto ">
+                            <div className="absolute z-50 top-12 left-0 bg-white shadow-xl border-t rounded-xl transition-all duration-500 ease-in-out min-w-[900px] w-auto">
                                 <div className="p-6">
                                     <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
                                         {items.map((category, index) => (
                                             <div key={index}>
                                                 <a
                                                     href={`/catalogo?category=${category.slug}`}
-                                                    className="customtext-neutral-dark font-bold text-sm mb-3 cursor-pointer hover:customtext-primary transition-colors duration-300 "
+                                                    className="customtext-neutral-dark font-bold text-sm mb-3 cursor-pointer hover:customtext-primary transition-colors duration-300"
                                                 >
                                                     {category.name}
                                                 </a>
                                                 <ul className="space-y-2">
-                                                    {category.subcategories.map(
-                                                        (item, itemIndex) => (
-                                                            <li key={itemIndex}>
-                                                                <a
-                                                                    href={`/catalogo?subcategory=${item.slug}`}
-                                                                    className=" customtext-neutral-dark text-sm hover:customtext-primary transition-colors duration-300 cursor-pointer"
-                                                                >
-                                                                    {item.name}
-                                                                </a>
-                                                            </li>
-                                                        )
-                                                    )}
+                                                    {category.subcategories.map((item, itemIndex) => (
+                                                        <li key={itemIndex}>
+                                                            <a
+                                                                href={`/catalogo?subcategory=${item.slug}`}
+                                                                className="customtext-neutral-dark text-sm hover:customtext-primary transition-colors duration-300 cursor-pointer"
+                                                            >
+                                                                {item.name}
+                                                            </a>
+                                                        </li>
+                                                    ))}
                                                 </ul>
                                             </div>
                                         ))}
@@ -68,7 +60,7 @@ const MenuSimple = ({ pages = [], items }) => {
                     </li>
                     {pages.map(
                         (page, index) =>
-                            page.menuable && ( // Simplified conditional rendering
+                            page.menuable && (
                                 <li key={index} className="py-3">
                                     <a
                                         href={page.path}
@@ -84,4 +76,5 @@ const MenuSimple = ({ pages = [], items }) => {
         </nav>
     );
 };
+
 export default MenuSimple;
